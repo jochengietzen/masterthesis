776850f40b009fafb42c68490dd1802625f5fd6c
Jochen Gietzen <jgietzen@inovex.de>
2020-02-24 20:49:53 +0900

    Create Datasets also automated for UCR and Others

--- CreateDatasets.py
+++ CreateDatasets.py
@@ -1,15 +1,17 @@
 from webapp.jgietzen.Data import Data
 from webapp.helper import log
-from webapp.config import dir_data, dir_rawdata
+from webapp.config import dir_data, dir_rawdata, dir_datafiles
 import dill as pkl
 from os.path import join
 import os
 import re
 import pandas as pd
+import numpy as np
 
-stdpath = join(dir_rawdata, 'artificialData')
+artificialData = join(dir_rawdata, 'artificialData')
+ucrAndRealworldData = join(dir_rawdata, 'ucrAndRealworld')
 
-def readFiles(path=stdpath):
+def readArtificialFiles(path=artificialData):
     files = [os.path.abspath(os.path.join(path, fil)) for fil in os.listdir(path) if '.csv' in fil]
     for fil in files:
         log('Reading', fil)
@@ -35,3 +37,27 @@ def readFiles(path=stdpath):
         data.save()
         log('Done')
     return data
+
+def readUcrAndRealworld(path=ucrAndRealworldData):
+    files = [os.path.abspath(os.path.join(path, fil)) for fil in os.listdir(path) if '.csv' in fil]
+    for fil in files:
+        log('Reading', fil)
+        df = pd.read_csv(fil)
+        if np.any([file2.endswith(re.sub('.csv', '', os.path.basename(fil))) for file2 in os.listdir(dir_datafiles)]):
+            continue
+        data = Data(df, column_sort='tsid',
+            filename = re.sub('.csv', '', os.path.basename(fil)),
+            originalfilename = os.path.basename(fil))
+        log('Set columns for', fil)
+        data.set_column_sort('time')
+        data.set_has_timestamp_value(False)
+        data.set_column_outlier([col for col in df.columns.tolist() if col in ['lof', 'isof', 'ee']])
+        data.set_relevant_columns([col for col in df.columns.tolist() if col not in ['tsid', 'time', 'lof', 'isof', 'ee']])
+        data.save()
+        log('Precalculate', fil)
+        data.precalculate()
+        data.precalculatePlots()
+        data.save()
+        log('Done')
+
+readUcrAndRealworld()
